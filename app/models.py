defbb34d9fa5f2d304af375aaa72e5e108da6578
mhgbtc <samdjido06@gmail.com>
2023-03-02 16:27:51 +0100

    Remote db config

--- app/models.py
+++ app/models.py
@@ -15,6 +15,7 @@ db = SQLAlchemy()
 # setup_db() : binds a flask app and a SQLAlchemy service
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
