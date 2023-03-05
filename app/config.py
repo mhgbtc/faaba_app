ddcba7f586dbd08d869f07b4278b9edaa1933982
Hans Tognon <learningway403@gmail.com>
2023-03-05 18:16:40 +0100

    updates

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@
 from os import getenv
 
 MAIL_SERVER = getenv('MAIL_SERVER')
-MAIL_PORT = getenv('MAIL_PORT')
+MAIL_PORT = int(getenv('MAIL_PORT', 587))
 MAIL_USERNAME = getenv('MAIL_USERNAME')
 MAIL_PASSWORD = getenv('MAIL_PASSWORD')
 MAIL_DEFAULT_SENDER = getenv('MAIL_DEFAULT_SENDER')
